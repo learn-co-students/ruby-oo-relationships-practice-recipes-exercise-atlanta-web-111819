6a696616e83be4cd2e20528457d6a281e6ed24f5
RobertTTaylor9517 <robt15@hotmail.com>
2019-11-25 15:48:08 -0500

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,19 @@
+class Ingredient
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+    end
+
+    def self.most_common_allergen
+        temp = Allergen.all.map{|alg| alg.ingredient}
+        temp.max_by{|ing| temp.count(ing)}
+    end
+
+end
